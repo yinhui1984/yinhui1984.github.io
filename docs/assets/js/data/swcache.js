ee5dc8e92f169c2886c5c632cb83a93dba1428cc
ZhouYinhui <g44784803@gmail.com>
2022-06-13 16:59:33 +0800

    auto updated by script

--- docs/assets/js/data/swcache.js
+++ docs/assets/js/data/swcache.js
@@ -24,19 +24,22 @@ const resource = [
   
   
     '/assets/img/favicons/android-chrome-192x192.png',
+    '/assets/img/favicons/android-chrome-384x384.png',
     '/assets/img/favicons/android-chrome-512x512.png',
     '/assets/img/favicons/apple-touch-icon.png',
     '/assets/img/favicons/favicon-16x16.png',
     '/assets/img/favicons/favicon-32x32.png',
     '/assets/img/favicons/favicon.ico',
     '/assets/img/favicons/mstile-150x150.png',
+    '/assets/img/favicons/safari-pinned-tab.svg',
     '/assets/js/dist/categories.min.js',
     '/assets/js/dist/commons.min.js',
     '/assets/js/dist/home.min.js',
     '/assets/js/dist/misc.min.js',
     '/assets/js/dist/page.min.js',
     '/assets/js/dist/post.min.js',
-    '/assets/js/dist/pvreport.min.js'
+    '/assets/js/dist/pvreport.min.js',
+    '/assets/img/favicons/.DS_Store'
 
 ];
 
--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220613.152641';
+const cacheName = 'chirpy-20220613.165634';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
