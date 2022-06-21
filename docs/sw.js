f781ca791333d1dcb535357091ea71c6014ef3c2
ZhouYinhui <g44784803@gmail.com>
2022-06-21 14:07:36 +0800

    auto updated by script

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220615.144057';
+const cacheName = 'chirpy-20220621.140704';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
